f0c800b47cf8375ae3c35de9260468c33403f835
barni363hun <barni.pbs@gmail.com>
2022-12-06 15:43:53 +0000

    3 feladat

--- doga_2/kavek/script.js
+++ doga_2/kavek/script.js
@@ -7,25 +7,45 @@ var db = {
     "lungo":0
 }
 
+var raktar = {
+    "cappuccino":1,
+    "espresso":5,
+    "latte":7,
+    "lungo":3
+}
+
+
 function kosarhozAd(name) {
-    if (name == "cappuccino") {
+    if (name == "cappuccino" && raktar[name]>0) {
         kosarErteke += 1600;
         db["cappuccino"]++;
-    } else if (name == "espresso") {
+        raktar["cappuccino"]--;
+    } else if (name == "espresso"&& raktar[name]>0) {
         kosarErteke += 1500;
         db["espresso"]++;
-    } else if (name == "latte") {
+        raktar["espresso"]--;
+    } else if (name == "latte"&& raktar[name]>0) {
         kosarErteke += 2000;
         db["latte"]++;
-    } else if (name == "lungo") {
+        raktar["latte"]--;
+    } else if (name == "lungo"&& raktar[name]>0) {
         kosarErteke += 2200;
         db["lungo"]++;
-
+        raktar["lungo"]--;
     }
+
+    if (raktar[name] == 0) {
+        alert("A termék elfogyott.")
+    }
+
     document.getElementById("cappuccino").innerHTML = db["cappuccino"] + " db"
     document.getElementById("espresso").innerHTML = db["espresso"] + " db"
     document.getElementById("latte").innerHTML = db["latte"] + " db"
     document.getElementById("lungo").innerHTML = db["lungo"] + " db"
+    document.getElementById("cappuccino2").innerHTML = raktar["cappuccino"] + " db"
+    document.getElementById("espresso2").innerHTML = raktar["espresso"] + " db"
+    document.getElementById("latte2").innerHTML = raktar["latte"] + " db"
+    document.getElementById("lungo2").innerHTML = raktar["lungo"] + " db"
 
     document.getElementById("kosarErteke").innerHTML =
         "Kosár értéke: " + kosarErteke + " Ft";
@@ -35,15 +55,19 @@ function kosarbolElvesz(name) {
     if (name == "cappuccino" && db["cappuccino"] >0) {
         kosarErteke -= 1600;
         db["cappuccino"]--;
+        raktar[name]++;
     } else if (name == "espresso" && db["espresso"] >0) {
         kosarErteke -= 1500;
         db["espresso"]--;
+        raktar[name]++;
     } else if (name == "latte" && db["latte"] >0) {
         kosarErteke -= 2000;
         db["latte"]--;
+        raktar[name]++;
     } else if (name == "lungo" && db["lungo"] >0) {
         kosarErteke -= 2200;
         db["lungo"]--;
+        raktar[name]++;
     }
     if (db[name] == 0) {
         var text = ""
@@ -62,6 +86,12 @@ function kosarbolElvesz(name) {
     document.getElementById("espresso").innerHTML = db["espresso"] + " db"
     document.getElementById("latte").innerHTML = db["latte"] + " db"
     document.getElementById("lungo").innerHTML = db["lungo"] + " db"
+    document.getElementById("cappuccino2").innerHTML = raktar["cappuccino"] + " db"
+    document.getElementById("espresso2").innerHTML = raktar["espresso"] + " db"
+    document.getElementById("latte2").innerHTML = raktar["latte"] + " db"
+    document.getElementById("lungo2").innerHTML = raktar["lungo"] + " db"
+    
     document.getElementById("kosarErteke").innerHTML =
         "Kosár értéke: " + kosarErteke + " Ft";
 }
+kosarhozAd("asd")
